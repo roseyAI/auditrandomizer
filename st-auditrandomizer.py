2cb6e656cd8ea0ee9dba0dbf12967927d60e29e2
Rose R <65215936+roseyAI@users.noreply.github.com>
2024-06-26 14:51:58 -0700

    removed timer sleep 1

--- st-auditrandomizer.py
+++ st-auditrandomizer.py
@@ -46,7 +46,6 @@ with col2:
             for percent_complete in range(100):
                 time.sleep(0.01)
                 my_bar.progress(percent_complete + 1, text=progress_text)
-            time.sleep(1)
             my_bar.empty() 
         # show results here
             with col2:
