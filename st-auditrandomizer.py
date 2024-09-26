f5509c9f2f0c76b16c9fd521b05313b0633d1a10
Rose R <65215936+roseyAI@users.noreply.github.com>
2024-09-26 16:45:37 -0700

    Update st-auditrandomizer.py

--- st-auditrandomizer.py
+++ st-auditrandomizer.py
@@ -21,9 +21,7 @@ col1, col2 = st.columns([4,4]) # make them as columns
 with col1: #place them in columns here
     st.header(" CE Ticket Randomizer ", divider='rainbow')
     with st.expander("Click here for more info"):
-        st. write("Our Random Sample Generator provides you with 5 hand-picked items from your list in seconds.
-                 This tool ensures that your samples are completely random. Ideal for quality assurance and data analysis,
-                 our generator allows you to confidently rely on the integrity of your sample data, making your decision-making process more efficient and accurate.")
+        st.write("Our Random Sample Generator provides you with 5 hand-picked items from your list in seconds. This tool ensures that your samples are completely random. Ideal for quality assurance and data analysis, our generator allows you to confidently rely on the integrity of your sample data, making your decision-making process more efficient and accurate.")
 
 # input from users, usually bulk data copied from Airtable
 with col2:
